916ecb7b3d21fcfd077145105826f43816ecbf0b
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-24 18:46:41 -0400

    middleware, checkusername

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,7 +1,7 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { findBy } = require("../users/users-model");
 
 const restricted = (req, res, next) => {
-  next();
   /*
     If the user does not provide a token in the Authorization header:
     status 401
@@ -33,7 +33,17 @@ const only = (role_name) => (req, res, next) => {
   */
 };
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
+  try {
+    const [user] = await findBy({ username: req.body.username });
+    if (!user) {
+      next({ status: 422, message: "Invalid credentials" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
   /*
     If the username in req.body does NOT exist in the database
     status 401
